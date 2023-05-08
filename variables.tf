96f53466f4b06320ebaa11c0567e0e7d8adc2944
Magz <michael.magner@gmail.com>
2023-05-08 12:20:02 -0400

    working aws deployment

---
+++ variables.tf
@@ -0,0 +1,49 @@
+# -----------------------------------------------------------------------------
+# Input Variables
+# -----------------------------------------------------------------------------
+variable "aws_region" {
+  type        = string
+  default = "us-east-1"
+}
+
+variable "name" {
+  description = "A name to use for resources."
+  type        = string
+  default = "magz"
+}
+
+variable "ecs_cpu" {
+  description = "The CPU count for the ECS task."
+  type        = number
+  default     = 256
+}
+
+variable "ecs_memory" {
+  description = "The memory count for the ECS task."
+  type        = number
+  default     = 512
+}
+
+variable "ecs_image" {
+  description = "The Docker image to use for the ECS task."
+  type        = string
+  default     = "magz999/todo"
+}
+
+variable "container_port" {
+  description = "The TCP port the container is listening on."
+  type        = number
+  default     = 3000
+}
+
+variable "permissions_boundary" {
+  description = "The ARN of an IAM permissions boundary to use for creating IAM resources."
+  type        = string
+  default     = null
+}
+
+variable "tags" {
+  description = "Map of common tags to apply to resources."
+  type        = map(any)
+  default     = {}
+}
