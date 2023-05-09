8048bc8387f4e8d3e68f02f8de9249be585ce239
Magz <michael.magner@gmail.com>
2023-05-08 21:45:27 -0400

    hardcode db pw

--- src/db.ts
+++ src/db.ts
@@ -3,7 +3,7 @@ import { Sequelize } from 'sequelize';
 export const sequelize = new Sequelize(
   process.env.PGDATABASE,
   process.env.PGUSER, 
-  process.env.PGPASSWORD, 
+  "foobarbaz", 
   {
     host: process.env.PGHOST,
     port: 5432,
